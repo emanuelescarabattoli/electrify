3144095717fa5f43b22f3b02ec2429c1079807dd
Emanuele Scarabattoli <emanuele.scarabattoli@evonove.it>
2019-06-26 11:45:12 +0200

    Small changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-const { app, BrowserWindow, Menu } = require("electron");
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
 const path = require("path");
 
-// Menu.setApplicationMenu(null);
+Menu.setApplicationMenu(null);
 
+// When the app is ready, a window is opened with the index page loaded
 app.on("ready", () => {
-    const window = new BrowserWindow({
+    const index = new BrowserWindow({
         width: 1000,
         height: 700,
         webPreferences: {
             nodeIntegration: true,
             devTools: true
-        }
+        },
+        fullscreen: true
     });
-    window.loadFile(path.join(__dirname, "index.html"));
+
+    // Here we load the index page of the application
+    index.loadFile(path.join(__dirname, "index.html"));
+
+    // On receiving the close message, the index window is closed
+    ipcMain.on("close-index-window", () => window.close());
 });
