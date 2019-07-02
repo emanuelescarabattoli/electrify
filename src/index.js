5ef227df17909fe4cf62de6d8ab56e11cfeccd4b
Emanuele Scarabattoli <emanuele.scarabattoli@evonove.it>
2019-07-02 18:06:25 +0200

    State management

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const { app, BrowserWindow, Menu, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 
-// Menu.setApplicationMenu(null);
+const { parseState } = require("./utils/parser");
 
 // When the app is ready, a window is opened with the index page loaded
 app.on("ready", () => {
@@ -21,4 +21,14 @@ app.on("ready", () => {
 
     // On receiving the close message, the index window is closed
     ipcMain.on("close-index-window", () => window.close());
+
+    // When gui wants to update the state a call to server is made
+    ipcMain.on("update-sensors-state", readSensorsState);
 });
+
+// TODO: Implement socket transmission
+// The function that update the sensors state by reading it from a socket
+const readSensorsState = (event, args) => {
+    const state = parseState("LIGHTGROUP1#ON;LIGHTGROUP2#OFF;LIGHTGROUP3#OFF;GATE#OFF")
+    event.sender.send("sensors-state-updated", state);
+}
---
+++ src/utils/parser.js
@@ -0,0 +1,26 @@
+
+// A function to parse the state string received from server to an object
+const parseState = stateString => {
+    let state = {};
+    const states = stateString.split(";");
+    return states.map(item => {
+        const stateValue = item.split("#");
+        switch (stateValue[0]) {
+            case "LIGHTGROUP1":
+                state.lightGroupOne = stateValue[1];
+                break;
+            case "LIGHTGROUP2":
+                state.lightGroupTwo = stateValue[1];
+                break;
+            case "LIGHTGROUP3":
+                state.lightGroupGate = stateValue[1];
+                break;
+            case "GATE":
+                state.gate = stateValue[1];
+                break;
+        }
+    });
+}
+
+// Export of utils
+module.exports = { parseState };
