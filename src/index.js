6ac9fb53f7953e60269360bea78b35297a3417fc
Emanuele Scarabattoli <emanuele.scarabattoli@evonove.it>
2019-07-03 17:58:41 +0200

    Client improvements

--- src/index.js
+++ src/index.js
@@ -4,11 +4,39 @@ const net = require("net");
 
 const { parseState } = require("./utils/parser");
 
+// The function that update the sensors state by reading it from a socket
+// here we start reading sensors state and update the GUI according to current state
+const readSensorsState = (event) => {
+    client.connect(7000, "localhost", onClientConnecte);
+    event.sender.send("sensors-state-updated", state);
+}
+
+// On client connected it starts to read sensors state
+const onClientConnecte = () => {
+    client.write("STATE");
+}
+
+// GUI is updated according to state
+const onClientReceiveData = data => {
+    if (data !== "DONE") {
+        state = parseState(data.toString());
+    }
+}
+
+// Logs the client errors
+const onClientError = error => {
+    console.error("Error " + error.message + " at " + new Date())
+}
+
 // Global state of sensors
 let state = {};
 
 // Creating the client object
-const client = new net.Socket()
+const client = new net.Socket();
+
+// Bind client event's to functions
+client.on("data", onClientReceiveData);
+client.on("error", onClientError);
 
 // When the app is ready, a window is opened with the index page loaded
 app.on("ready", () => {
@@ -29,21 +57,3 @@ app.on("ready", () => {
     // Start receiving signals from GUI
     ipcMain.on("update-sensors-state", readSensorsState);
 });
-
-// The function that update the sensors state by reading it from a socket
-// here we start reading sensors state and update the GUI according to current state
-const readSensorsState = (event, args) => {
-    client.connect(7000, "localhost", onClientConnected);
-    event.sender.send("sensors-state-updated", state);
-}
-
-// On client connected it starts to read sensors state
-const onClientConnected = () => {
-    client.write("STATE");
-    client.on("data", onClientReceiveData)
-}
-
-// GUI is updated according to state
-const onClientReceiveData = data => {
-    state = parseState(data.toString());
-}
