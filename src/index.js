6714980c982968647562844c070308ae21c11497
Emanuele Scarabattoli <em.scarabattoli@gmail.com>
2019-07-03 21:20:06 +0200

    Send and receive state

--- src/index.js
+++ src/index.js
@@ -4,35 +4,43 @@ const net = require("net");
 
 const { parseState } = require("./utils/parser");
 
+// Global state of sensors
+let state = {};
+
+// Last message to send to server
+let message = "STATE";
+
+// Creating the client object to read status from server
+const client = new net.Socket();
+
 // The function that update the sensors state by reading it from a socket
 // here we start reading sensors state and update the GUI according to current state
-const readSensorsState = (event) => {
-    client.connect(7000, "localhost", onClientConnecte);
+const syncSensorsState = (event) => {
+    client.connect(7000, "localhost", onClientConnect);
     event.sender.send("sensors-state-updated", state);
 }
 
-// On client connected it starts to read sensors state
-const onClientConnecte = () => {
-    client.write("STATE");
+// On client connected it starts to ask for sensors state
+// if there is a message to send to server it will be sent
+const onClientConnect = () => {
+    client.write(message);
+    message = "STATE";
 }
 
 // GUI is updated according to state
 const onClientReceiveData = data => {
-    if (data !== "DONE") {
-        state = parseState(data.toString());
-    }
+    state = parseState(data.toString());
 }
 
 // Logs the client errors
 const onClientError = error => {
-    console.error("Error " + error.message + " at " + new Date())
+    console.error("Error on client read " + error.message + " at " + new Date())
 }
 
-// Global state of sensors
-let state = {};
-
-// Creating the client object
-const client = new net.Socket();
+// The function that send a message to server to update the state of a sensor
+const writeSensorState = (event, data) => {
+    message = data;
+}
 
 // Bind client event's to functions
 client.on("data", onClientReceiveData);
@@ -55,5 +63,8 @@ app.on("ready", () => {
     index.loadFile(path.join(__dirname, "index.html"));
 
     // Start receiving signals from GUI
-    ipcMain.on("update-sensors-state", readSensorsState);
+    ipcMain.on("update-sensors-state", syncSensorsState);
+
+    // When GUI wants to update the status of a sensor a message is sent
+    ipcMain.on("send-sensor-state", writeSensorState);
 });
